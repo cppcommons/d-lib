8e058f1b8170d0cc018edf32a5c5b95807480398
root <root@super-computer>
2017-11-24 07:47:48 +0900

    @

--- my-prepare-64.bat
+++ my-prepare-64.bat
@@ -1,8 +1,7 @@
 setlocal
 call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" amd64
 echo on
-if not exist sqlite-win-64bit-3210000-ms64.lib (
-  lib /def:sqlite-win-64bit-3210000.def /machine:x64 /out:sqlite-win-64bit-3210000-ms64.lib
- )
+lib /def:sqlite-win-64bit-3210000.def /machine:x64 /out:sqlite-win-64bit-3210000-ms64.lib
+lib /def:libcurl-ms64.def /machine:x64 /out:libcurl-ms64.lib
 endlocal
 pause
